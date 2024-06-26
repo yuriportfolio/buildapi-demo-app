8fd0f6f1566f6942fb384199c1438026ab2cc365
John Doe <gagarinloops@gmail.com>
2024-06-26 23:30:04 +0200

    firebase revert

--- src/lib/firebase.js
+++ src/lib/firebase.js
@@ -3,9 +3,9 @@ import 'firebase/compat/database';
 import 'firebase/compat/auth';
 
 const firebaseConfig = {
-    apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
-    authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
-    projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+    apiKey: 'AIzaSyAf8rJqbViS8IumsRmdUW_VaBM2hYie0kM',
+    authDomain: 'withai-185a8.firebaseapp.com',
+    projectId: 'withai-185a8',
 };
 
 if (!firebase.apps.length) {
